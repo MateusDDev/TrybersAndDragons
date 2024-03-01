c91e6aa902fa5f7c04668c58603ceef6d5058e15
MateusDDev <ms4811978@gmail.com>
2024-03-01 10:24:56 -0300

    feat: requisito 5

---
+++ src/Archetypes/Mage.ts
@@ -0,0 +1,21 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Mage extends Archetype {
+  private _energyType: EnergyType = 'mana';
+  private static _instances = 0;
+
+  constructor(name: string) {
+    super(name);
+
+    Mage._instances += 1;
+  }
+
+  static createdArchetypeInstances(): number {
+    return this._instances;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
---
+++ src/Archetypes/Ranger.ts
@@ -0,0 +1,21 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Ranger extends Archetype {
+  private _energyType: EnergyType = 'stamina';
+  private static _instances = 0;
+
+  constructor(name: string) {
+    super(name);
+
+    Ranger._instances += 1;
+  }
+
+  static createdArchetypeInstances(): number {
+    return this._instances;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
